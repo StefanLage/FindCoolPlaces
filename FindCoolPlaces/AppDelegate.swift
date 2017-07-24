7a561b48f7094752656cda8bd86dbd7320a985ed
Stefan Lage <lagestfan@gmail.com>
2017-07-24 19:22:06 +0100

    Init project

---
+++ FindCoolPlaces/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FindCoolPlaces
+//
+//  Created by Stefan Lage on 24/07/2017.
+//  Copyright © 2017 Stefan Lage. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
